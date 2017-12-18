b286c44416b20413c0766197bc6e53b9975c26c3
pussingh <pussingh@qasource.com>
2017-12-18 19:03:39 +0530

    Notes Application (Performs crud operations) Adding notes , Removing notes , Filtering notes

---
+++ NoteApplication/app.js
@@ -0,0 +1,38 @@
+console.log('************* Starting app.js **********');
+
+const yargs = require('yargs');
+const lodash = require('lodash');
+const notes = require('./notes.js');
+
+const argv = yargs.argv; // to get arguments from CLI
+
+var command = argv._[0]; // gettig first arguement 
+// console.log('args',args);
+
+
+if(command === 'add'){
+	var note = notes.addNote(argv.title,argv.body);
+	if(note){
+		console.log('Note Created');
+		notes.logNote(note);
+	}
+	else {
+		console.log("Note not Created");
+	}
+
+}else if(command === 'remove'){
+	var removedNote = notes.removeNote(argv.title);
+	if(removedNote){
+		console.log('Note is removed');
+	}else {
+		console.log('Note not found');
+	}
+}else if(command === 'read'){
+	var readedNote = notes.getNote(argv.title);
+	if(readedNote){
+		console.log('Note is Exists : ');
+		notes.logNote(readedNote);
+	}else {
+		console.log('Note not exist');
+	}
+}
---
+++ NoteApplication/notes.js
@@ -0,0 +1,72 @@
+const fs = require('fs');
+
+// code to fetch all notes from file and use this fn inside of notes.js
+var fetchNotes = () => {
+	try{
+		var notesString = fs.readFileSync('notes-data.json');
+	    notes = JSON.parse(notesString);
+	    return notes;
+		console.log('notes:',notes)
+		}catch(e){
+			return [];
+		}
+};
+
+// code to save the notes and used this function inside of notes.js
+var saveNotes = (notes) => {
+	fs.writeFileSync('notes-data.json' , JSON.stringify(notes));
+};
+
+// code to add notes
+var addNote = (title , body) => {
+ var notes = fetchNotes();
+ var note = {
+ 	title,
+ 	body
+ };
+
+// to check the duplicate notes filter() is userd as arrow function
+var duplicateNotes = notes.filter((note) => note.title === title);
+if(duplicateNotes.length === 0){
+		notes.push(note);
+		saveNotes(notes);
+		return note;
+		console.log('File is ready')			
+	}
+	else{
+		console.log('Duplicate exittss',note.title);
+	}
+
+};
+
+ // code to remove notes
+var removeNote = (title) => {
+	var notes = fetchNotes(); //get all notes first
+	var filterdNotes = notes.filter((note) => note.title !== title) // filter notes according to the title argument
+	saveNotes(filterdNotes); // after removing saving the array of new notes
+	 	
+ 	return notes.length != filterdNotes.length;
+ };
+ // code to read Notes
+ var getNote = (title) => {
+ 	var notes = fetchNotes(); //fetch the list first
+ 	var filteredNotes = notes.filter((note) => note.title === title) // get filteredNotes
+ 	return filteredNotes[0]; //if note exist than return otherwise return undefind
+ }
+
+// code to call logged note
+ var logNote = (note) =>{
+ 	console.log('--');
+	console.log(`Body : ${note.body}`);
+	console.log(`Title: ${note.title}`);
+ }
+
+
+
+// Mendatory to add every function in exports to call in the main code
+module.exports = {
+	addNote:addNote,
+	removeNote:removeNote,
+	getNote:getNote,
+	logNote:logNote
+	}
